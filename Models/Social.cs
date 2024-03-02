24ec9c9c32450c42993b371a241e2f0525ffb656
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2024-03-02 11:06:48 +0530

    About Page
    Logo Added , Landing Page Modification
    Prism Js Introduced for Syntax Highlighting
    Social Media Inclusion
    
    // From Now on commit will be frequently.
    because of my potato pc is not bale handle more than three chrome tabs and Shut Down frequently and I loose my Temper

---
+++ Models/Social.cs
@@ -0,0 +1,10 @@
+﻿namespace CatLikeCoding.Models
+{
+    public class Social
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+
+        public string TargetLink { get; set; }
+    }
+}
