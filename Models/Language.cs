fa9bd5abb656d1853426925c3ff41329667e49cd
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2024-03-04 14:45:11 +0530

    Basic Clock Learning

---
+++ Models/Language.cs
@@ -0,0 +1,13 @@
+﻿namespace CatLikeCoding.Models
+{
+    public enum Language
+    {
+        csharp, 
+        cs,
+        dotnet,
+        hlsl,
+        gdscript,
+        glsl
+    }
+    // for more add from https://prismjs.com/
+}
